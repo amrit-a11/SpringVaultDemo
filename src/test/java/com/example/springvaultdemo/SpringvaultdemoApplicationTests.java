2da04b7f519972486cf003b20790c4669fd34764
amrit-a11 <apamritpanda@gmail.com>
2024-12-11 14:52:51 +0530

    initial commit

---
+++ src/test/java/com/example/springvaultdemo/SpringvaultdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvaultdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvaultdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
